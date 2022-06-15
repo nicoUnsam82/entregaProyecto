cb0688306838b893556dae288a5b9044743c1f73
nicoUnsam82 <nlgonzalez@estudiantes.unsam.edu.ar>
2022-06-15 16:11:41 -0300

    correccion puerto 2

--- servidor.js
+++ servidor.js
@@ -8,7 +8,7 @@ export function createServer({ port = 8080 }, app, httpServer) {
         app.get('/servidor', (req, res, next) => {
             res.send(`PROCESO '${process.pid}' ESCUCHANDO EN ${server.address().port}`)
         })
-        server = httpServer.listen(process.env.port || 8080, () => {
+        server = httpServer.listen(process.env.PORT || 8080, () => {
 
             logger.info(`PROCESO '${process.pid}' INICIO SERVIDOR: http://localhost:${httpServer.address().port}`)
             resolve(server)
